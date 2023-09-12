dc6f8cfd8016adaba18a5a947028f4894175e82e
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2023-09-12 11:39:29 -0600

    Add front

--- front/src/components/Form.jsx
+++ front/src/components/Form.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
-import CustomInput from './CustomInput';
 
 // Define los nombres y placeholders de los campos
 const inputFields = [
-  { name: 'index', placeholder: 'Index', type: '', value: '' },
-  { name: 'trend', placeholder: 'Trend', type: '', value: '' },
-  { name: 'sin(1,freq=W-SUN)', placeholder: 'sin(1,freq=W-SUN)', type: '', value: '' },
-  { name: 'cos(1,freq=W-SUN)', placeholder: 'cos(1,freq=W-SUN)', type: '', value: '' },
-  { name: 'sin(2,freq=W-SUN)', placeholder: 'sin(2,freq=W-SUN)', type: '', value: '' },
-  { name: 'cos(2,freq=W-SUN)', placeholder: 'cos(2,freq=W-SUN)', type: '', value: '' },
-  { name: 'sin(3,freq=W-SUN)', placeholder: 'sin(3,freq=W-SUN)', type: '', value: '' },
-  { name: 'cos(3,freq=W-SUN)', placeholder: 'cos(3,freq=W-SUN)', type: '', value: '' },
-  { name: 'Average_oil', placeholder: 'Average_oil', type: '', value: '' },
-  { name: 'week_day', placeholder: 'week_day', type: '', value: '' },
-  { name: 'dayofweek_1', placeholder: 'dayofweek_1', type: '', value: '' },
-  { name: 'dayofweek_2', placeholder: 'dayofweek_2', type: '', value: '' },
-  { name: 'dayofweek_3', placeholder: 'dayofweek_3', type: '', value: '' },
-  { name: 'dayofweek_4', placeholder: 'dayofweek_4', type: '', value: '' },
-  { name: 'dayofweek_5', placeholder: 'dayofweek_5', type: '', value: '' },
-  { name: 'dayofweek_6', placeholder: 'dayofweek_6', type: '', value: '' },
-  { name: 'type_Additional', placeholder: 'type_Additional', type: '', value: '' },
-  { name: 'type_Bridge', placeholder: 'type_Bridge', type: '', value: '' },
-  { name: 'type_Event', placeholder: 'type_Event', type: '', value: '' },
-  { name: 'type_Holiday', placeholder: 'type_Holiday', type: '', value: '' },
-  { name: 'type_Transfer', placeholder: 'type_Transfer', type: '', value: '' },
+  { name: 'trend', placeholder: 'Trend', type: Number, value: 1.0 },
+  { name: 'sin(1,freq=W-SUN)', placeholder: 'sin(1,freq=W-SUN)', type: Number, value: -0.7818314824680299 },
+  { name: 'cos(1,freq=W-SUN)', placeholder: 'cos(1,freq=W-SUN)', type: Number, value: 0.6234898018587334 },
+  { name: 'sin(2,freq=W-SUN)', placeholder: 'sin(2,freq=W-SUN)', type: Number, value: -0.9749279121818235 },
+  { name: 'cos(2,freq=W-SUN)', placeholder: 'cos(2,freq=W-SUN)', type: Number, value: -0.2225209339563148 },
+  { name: 'sin(3,freq=W-SUN)', placeholder: 'sin(3,freq=W-SUN)', type: Number, value: -0.43388373911755757 },
+  { name: 'cos(3,freq=W-SUN)', placeholder: 'cos(3,freq=W-SUN)', type: Number, value: -0.9009688679024194 },
+  { name: 'Average_oil', placeholder: 'Average_oil', type: Number, value: 49.15428571428572 },
+  { name: 'week_day', placeholder: 'week_day', type: Number, value: 0 },
+  { name: 'dayofweek_1', placeholder: 'dayofweek_1', type: Boolean, value: 0 },
+  { name: 'dayofweek_2', placeholder: 'dayofweek_2', type: Boolean, value: 0 },
+  { name: 'dayofweek_3', placeholder: 'dayofweek_3', type: Boolean, value: 0 },
+  { name: 'dayofweek_4', placeholder: 'dayofweek_4', type: Boolean, value: 0 },
+  { name: 'dayofweek_5', placeholder: 'dayofweek_5', type: Boolean, value: 0 },
+  { name: 'dayofweek_6', placeholder: 'dayofweek_6', type: Boolean, value: 1 },
+  { name: 'type_Additional', placeholder: 'type_Additional', type: Boolean, value: 0 },
+  { name: 'type_Bridge', placeholder: 'type_Bridge', type: Boolean, value: 0 },
+  { name: 'type_Event', placeholder: 'type_Event', type: Boolean, value: 0 },
+  { name: 'type_Holiday', placeholder: 'type_Holiday', type: Boolean, value: 0 },
+  { name: 'type_Transfer', placeholder: 'type_Transfer', type: Boolean, value: 0 },
+  { name: 'type_Work Day', placeholder: 'type_Work Day', type: Boolean, value: 0 },
 ];
+
 
 
 const Form = () => {
@@ -44,15 +44,22 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-md mx-auto">
       {inputFields.map((field) => (
-        <input
-          key={field.name}
-          type="text"
-          placeholder={field.placeholder}
-          name={field.name}
-          value={formData[field.name] || ''}
-          onChange={handleChange}
-          className="border rounded-md py-2 px-4 my-2"
-        />
+		<div class="mb-6 ml-10">
+			<div>
+				<label class="bg-blue-200 text-black font-bold py-2 px-4 rounded">
+				{field.name}
+				</label>
+			</div>
+			
+			<input
+			key={field.name}
+			type={field.type}
+			placeholder={field.placeholder}
+			name={field.name}
+			value={field.value}
+			className="border rounded-md py-2 px-4 my-2"
+			/>
+		</div>
       ))}
       <div className="text-center">
         <button
--- front/src/pages/ProjectInfo.jsx
+++ front/src/pages/ProjectInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 import Form from '../components/Form';
 
@@ -12,28 +12,74 @@ const options = {
     id: 'basic-bar',
   },
   xaxis: {
-    categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+    categories: [
+      'AUTOMOTIVE', 'BABY CARE', 'BEAUTY', 'BEVERAGES', 'BOOKS', 'BREAD/BAKERY', 'CELEBRATION', 'CLEANING', 'DAIRY', 'DELI', 'EGGS', 'FROZEN FOODS', 'GROCERY', 'GROCERY II', 'HARDWARE', 'HOME AND KITCHEN I', 'HOME AND KITCHEN II', 'HOME APPLIANCES', 'HOME CARE', 'LADIESWEAR'
+    ],
   },
+  colors: [
+    '#FF5733',
+    '#33FF6E',
+    '#3366FF',
+    '#FF33A8',
+    '#FFFF33',
+    '#33FFFA',
+    '#FF3366',
+    '#33FF33',
+    '#FF5733',
+    '#33FF6E',
+    '#3366FF',
+    '#FF33A8',
+    '#FFFF33',
+    '#33FFFA',
+    '#FF3366',
+    '#33FF33',
+    '#FF5733',
+    '#33FF6E',
+    '#3366FF',
+    '#FF33A8',
+  ],
 };
 
-const series = [
+const initialSeries = [
   {
     name: 'Ventas',
-    data: [100, 150, 200, 180, 220],
+    data: [200, 300, 500, 200, 500, 50, 700, 300, 500, 500, 500, 100, 100, 500, 0, 20, 500, 500, 500, 500],
   },
 ];
 
 const ProjectInfo = () => {
+  const [chartType, setChartType] = useState('bar'); // Estado para el tipo de gráfico
+  const [chartKey, setChartKey] = useState(0); // Estado para forzar la actualización del gráfico
+
+  // Función para cambiar entre los tipos de gráficos
+  const toggleChartType = () => {
+    setChartKey(chartKey + 1);
+    setChartType(chartType === 'bar' ? 'line' : 'bar');
+  };
+
   return (
     <div className={`min-h-screen flex ${corporateColors.background}`}>
-	  <div className="overflow-y-auto h-full w-1/3 p-6 rounded shadow-md bg-white">
-		<Form />
-	  </div>
-      <div className="flex-1 p-6 rounded shadow-md bg-white">
-        <h1 className="text-3xl font-semibold mb-6">Análisis de Ventas</h1>
-		<ReactApexChart options={options} series={series} type="bar" height={350} />
+      <div className="overflow-y-auto h-full w-1/3 p-6 rounded shadow-md bg-white">
+        <Form />
       </div>
-	  
+      <div className="flex-1 p-6 rounded shadow-md bg-white">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-semibold mb-6">Análisis de Ventas ({chartType === 'line' ? 'Line' : 'Bar'})</h1>
+          <button
+            className={`p-2 rounded ${corporateColors.button}`}
+            onClick={toggleChartType}
+          >
+            Cambiar Tipo de Gráfico
+          </button>
+        </div>
+        <ReactApexChart
+          key={chartKey} // Esto forzará la actualización del gráfico
+          options={options}
+          series={initialSeries}
+          type={chartType}
+          height={350}
+        />
+      </div>
     </div>
   );
 };
